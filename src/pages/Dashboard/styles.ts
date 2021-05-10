60aebbe0d5b14d4026afe80aa02845e3cddd676f
jm4rcos <joaomarcosmacimiro@gmail.com>
2021-05-10 02:01:50 -0300

    update

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,9 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { shade } from 'polished'
+
+interface FormProps {
+  hasError: boolean
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -8,9 +13,10 @@ export const Title = styled.h1`
 
   margin-top: 80px;
 `
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
+  margin-bottom: 80px;
 
   display: flex;
 
@@ -20,6 +26,16 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    border: 2px solid #fff;
+
+    ${(props) => props.hasError && css`
+      border-color: crimson;
+    `}
+
+    &::placeholder{
+      color: #a8a8b3;
+    }
   }
 
   button{
@@ -29,10 +45,56 @@ export const Form = styled.form`
     border: 0;
     color: #fff;
     font-weight: bold;
-
+    transition: background-color 0.2s;
 
     &:hover{
-      background: #
+      background: ${shade(0.2, '#04d361')}
     }
+  }
+`
+export const Repositories = styled.div`
+  margin-top: 16px;
+  max-width: 700px;
+
+  a{
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    &:hover{
+      transform: translateX(10px);      
+    }
+
+    img{
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div{
+      margin-left: 16px;
+
+      strong{
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p{
+          font-size: 18px;
+          color: #aba8b3;
+          margin-top: 4px;
+            
+      }
+    }
+    svg{
+        margin-left: auto;
+        color: #cbcbd6;
+      }
   }
 `
